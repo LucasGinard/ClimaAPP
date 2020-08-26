fb8fb5f3608efedd2dc5e0f56f7767b51e95d783
LucasGinard <lucasmatiasginard@hotmail.es>
2020-08-26 17:59:45 -0400

    Fase 0.4 Mejorando UI

--- app/src/main/java/com/example/climaapp/MainActivity.kt
+++ app/src/main/java/com/example/climaapp/MainActivity.kt
@@ -13,34 +13,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.setDisplayShowHomeEnabled(true);
         supportActionBar?.setIcon(R.mipmap.ic_launcher);
-
+        var apikey = "95d164075f415ce1eff1ab7453be71bd"
         pasar.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
-                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=asuncion&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=asuncion&units=metric&lang=sp%22&appid=${apikey}")
             }
             startActivity(intent)
         }
         pasar2.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
-                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Ciudad%20del%20Este,%20PY&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Ciudad%20del%20Este,%20PY&units=metric&lang=sp&appid=${apikey}")
             }
             startActivity(intent)
         }
         pasar3.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
-                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=encarnacion&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=encarnacion&units=metric&lang=sp&appid=${apikey}")
             }
             startActivity(intent)
         }
         pasar4.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
-                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Loma%20Plata,%20PY&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Loma%20Plata,%20PY&units=metric&lang=sp&appid=${apikey}")
             }
             startActivity(intent)
         }
         pasar5.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
-                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Villarrica&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Villarrica&units=metric&lang=sp&appid=${apikey}")
             }
             startActivity(intent)
         }
