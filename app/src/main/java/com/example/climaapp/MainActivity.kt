55d18b05cff5ee994ea93a074b04262dc1bd37b0
LucasGinard <lucasmatiasginard@hotmail.es>
2020-08-26 16:57:09 -0400

    Fase 0.3 Funcional

--- app/src/main/java/com/example/climaapp/MainActivity.kt
+++ app/src/main/java/com/example/climaapp/MainActivity.kt
@@ -22,7 +22,25 @@ class MainActivity : AppCompatActivity() {
         }
         pasar2.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Ciudad%20del%20Este,%20PY&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+            }
+            startActivity(intent)
+        }
+        pasar3.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java).apply {
                 putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=encarnacion&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+            }
+            startActivity(intent)
+        }
+        pasar4.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java).apply {
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Loma%20Plata,%20PY&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
+            }
+            startActivity(intent)
+        }
+        pasar5.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java).apply {
+                putExtra("ubicacion","https://api.openweathermap.org/data/2.5/weather?q=Villarrica&appid=95d164075f415ce1eff1ab7453be71bd&units=metric&lang=sp")
             }
             startActivity(intent)
         }
